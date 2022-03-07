5f379431c33c770eba1f7fe02d98fd20b032bc42
Pro-salud <iititulacion@gmail.com>
2022-03-06 18:36:42 -0600

    Backend

---
+++ easyappointments/storage/logs/log-2022-03-06.php
@@ -0,0 +1,68 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2022-03-06 07:19:38 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'root'@'localhost' (using password: YES) C:\xampp\htdocs\pro-salud.github.io\easyappointments\vendor\codeigniter\framework\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2022-03-06 07:19:38 --> Severity: error --> Exception: Unable to connect to the database. C:\xampp\htdocs\pro-salud.github.io\easyappointments\vendor\codeigniter\framework\system\database\DB_driver.php 433
+ERROR - 2022-03-06 08:17:11 --> Email could not been sent. Mailer Error (Line 166): Could not instantiate mail function.
+ERROR - 2022-03-06 08:17:11 --> #0 C:\xampp\htdocs\pro-salud.github.io\easyappointments\application\libraries\Notifications.php(94): EA\Engine\Notifications\Email->send_appointment_details(Array, Array, Array, Array, Array, Object(EA\Engine\Types\Text), Object(EA\Engine\Types\Text), Object(EA\Engine\Types\Url), Object(EA\Engine\Types\Email), Object(EA\Engine\Types\Text), 'America/Mexico_...')
+#1 C:\xampp\htdocs\pro-salud.github.io\easyappointments\application\controllers\Appointments.php(490): Notifications->notify_appointment_saved(Array, Array, Array, Array, Array, false)
+#2 C:\xampp\htdocs\pro-salud.github.io\easyappointments\vendor\codeigniter\framework\system\core\CodeIgniter.php(481): Appointments->ajax_register_appointment()
+#3 C:\xampp\htdocs\pro-salud.github.io\easyappointments\index.php(341): require_once('C:\\xampp\\htdocs...')
+#4 {main}
+ERROR - 2022-03-06 23:02:39 --> Severity: Warning --> Trying to access array offset on value of type null C:\xampp\htdocs\pro-salud.github.io\easyappointments\application\controllers\Backend_api.php 164
+ERROR - 2022-03-06 23:02:39 --> Severity: Warning --> Trying to access array offset on value of type null C:\xampp\htdocs\pro-salud.github.io\easyappointments\application\controllers\Backend_api.php 164
+ERROR - 2022-03-06 23:03:39 --> Severity: Warning --> Trying to access array offset on value of type null C:\xampp\htdocs\pro-salud.github.io\easyappointments\application\controllers\Backend_api.php 164
+ERROR - 2022-03-06 23:03:39 --> Severity: Warning --> Trying to access array offset on value of type null C:\xampp\htdocs\pro-salud.github.io\easyappointments\application\controllers\Backend_api.php 164
+ERROR - 2022-03-06 23:04:39 --> Severity: Warning --> Trying to access array offset on value of type null C:\xampp\htdocs\pro-salud.github.io\easyappointments\application\controllers\Backend_api.php 164
+ERROR - 2022-03-06 23:04:39 --> Severity: Warning --> Trying to access array offset on value of type null C:\xampp\htdocs\pro-salud.github.io\easyappointments\application\controllers\Backend_api.php 164
+ERROR - 2022-03-06 23:05:39 --> Severity: Warning --> Trying to access array offset on value of type null C:\xampp\htdocs\pro-salud.github.io\easyappointments\application\controllers\Backend_api.php 164
+ERROR - 2022-03-06 23:05:39 --> Severity: Warning --> Trying to access array offset on value of type null C:\xampp\htdocs\pro-salud.github.io\easyappointments\application\controllers\Backend_api.php 164
+ERROR - 2022-03-06 23:06:39 --> Severity: Warning --> Trying to access array offset on value of type null C:\xampp\htdocs\pro-salud.github.io\easyappointments\application\controllers\Backend_api.php 164
+ERROR - 2022-03-06 23:06:39 --> Severity: Warning --> Trying to access array offset on value of type null C:\xampp\htdocs\pro-salud.github.io\easyappointments\application\controllers\Backend_api.php 164
+ERROR - 2022-03-06 23:07:39 --> Severity: Warning --> Trying to access array offset on value of type null C:\xampp\htdocs\pro-salud.github.io\easyappointments\application\controllers\Backend_api.php 164
+ERROR - 2022-03-06 23:07:39 --> Severity: Warning --> Trying to access array offset on value of type null C:\xampp\htdocs\pro-salud.github.io\easyappointments\application\controllers\Backend_api.php 164
+ERROR - 2022-03-06 23:08:39 --> Severity: Warning --> Trying to access array offset on value of type null C:\xampp\htdocs\pro-salud.github.io\easyappointments\application\controllers\Backend_api.php 164
+ERROR - 2022-03-06 23:08:39 --> Severity: Warning --> Trying to access array offset on value of type null C:\xampp\htdocs\pro-salud.github.io\easyappointments\application\controllers\Backend_api.php 164
+ERROR - 2022-03-06 23:09:39 --> Severity: Warning --> Trying to access array offset on value of type null C:\xampp\htdocs\pro-salud.github.io\easyappointments\application\controllers\Backend_api.php 164
+ERROR - 2022-03-06 23:09:39 --> Severity: Warning --> Trying to access array offset on value of type null C:\xampp\htdocs\pro-salud.github.io\easyappointments\application\controllers\Backend_api.php 164
+ERROR - 2022-03-06 23:10:39 --> Severity: Warning --> Trying to access array offset on value of type null C:\xampp\htdocs\pro-salud.github.io\easyappointments\application\controllers\Backend_api.php 164
+ERROR - 2022-03-06 23:10:39 --> Severity: Warning --> Trying to access array offset on value of type null C:\xampp\htdocs\pro-salud.github.io\easyappointments\application\controllers\Backend_api.php 164
+ERROR - 2022-03-06 23:11:39 --> Severity: Warning --> Trying to access array offset on value of type null C:\xampp\htdocs\pro-salud.github.io\easyappointments\application\controllers\Backend_api.php 164
+ERROR - 2022-03-06 23:11:39 --> Severity: Warning --> Trying to access array offset on value of type null C:\xampp\htdocs\pro-salud.github.io\easyappointments\application\controllers\Backend_api.php 164
+ERROR - 2022-03-06 23:12:39 --> Severity: Warning --> Trying to access array offset on value of type null C:\xampp\htdocs\pro-salud.github.io\easyappointments\application\controllers\Backend_api.php 164
+ERROR - 2022-03-06 23:12:39 --> Severity: Warning --> Trying to access array offset on value of type null C:\xampp\htdocs\pro-salud.github.io\easyappointments\application\controllers\Backend_api.php 164
+ERROR - 2022-03-06 23:13:39 --> Severity: Warning --> Trying to access array offset on value of type null C:\xampp\htdocs\pro-salud.github.io\easyappointments\application\controllers\Backend_api.php 164
+ERROR - 2022-03-06 23:13:39 --> Severity: Warning --> Trying to access array offset on value of type null C:\xampp\htdocs\pro-salud.github.io\easyappointments\application\controllers\Backend_api.php 164
+ERROR - 2022-03-06 23:14:39 --> Severity: Warning --> Trying to access array offset on value of type null C:\xampp\htdocs\pro-salud.github.io\easyappointments\application\controllers\Backend_api.php 164
+ERROR - 2022-03-06 23:14:39 --> Severity: Warning --> Trying to access array offset on value of type null C:\xampp\htdocs\pro-salud.github.io\easyappointments\application\controllers\Backend_api.php 164
+ERROR - 2022-03-06 23:15:39 --> Severity: Warning --> Trying to access array offset on value of type null C:\xampp\htdocs\pro-salud.github.io\easyappointments\application\controllers\Backend_api.php 164
+ERROR - 2022-03-06 23:15:39 --> Severity: Warning --> Trying to access array offset on value of type null C:\xampp\htdocs\pro-salud.github.io\easyappointments\application\controllers\Backend_api.php 164
+ERROR - 2022-03-06 23:16:39 --> Severity: Warning --> Trying to access array offset on value of type null C:\xampp\htdocs\pro-salud.github.io\easyappointments\application\controllers\Backend_api.php 164
+ERROR - 2022-03-06 23:16:39 --> Severity: Warning --> Trying to access array offset on value of type null C:\xampp\htdocs\pro-salud.github.io\easyappointments\application\controllers\Backend_api.php 164
+ERROR - 2022-03-06 23:17:39 --> Severity: Warning --> Trying to access array offset on value of type null C:\xampp\htdocs\pro-salud.github.io\easyappointments\application\controllers\Backend_api.php 164
+ERROR - 2022-03-06 23:17:39 --> Severity: Warning --> Trying to access array offset on value of type null C:\xampp\htdocs\pro-salud.github.io\easyappointments\application\controllers\Backend_api.php 164
+ERROR - 2022-03-06 23:18:39 --> Severity: Warning --> Trying to access array offset on value of type null C:\xampp\htdocs\pro-salud.github.io\easyappointments\application\controllers\Backend_api.php 164
+ERROR - 2022-03-06 23:18:39 --> Severity: Warning --> Trying to access array offset on value of type null C:\xampp\htdocs\pro-salud.github.io\easyappointments\application\controllers\Backend_api.php 164
+ERROR - 2022-03-06 23:22:39 --> Severity: Warning --> Trying to access array offset on value of type null C:\xampp\htdocs\pro-salud.github.io\easyappointments\application\controllers\Backend_api.php 164
+ERROR - 2022-03-06 23:22:39 --> Severity: Warning --> Trying to access array offset on value of type null C:\xampp\htdocs\pro-salud.github.io\easyappointments\application\controllers\Backend_api.php 164
+ERROR - 2022-03-06 23:23:39 --> Severity: Warning --> Trying to access array offset on value of type null C:\xampp\htdocs\pro-salud.github.io\easyappointments\application\controllers\Backend_api.php 164
+ERROR - 2022-03-06 23:23:39 --> Severity: Warning --> Trying to access array offset on value of type null C:\xampp\htdocs\pro-salud.github.io\easyappointments\application\controllers\Backend_api.php 164
+ERROR - 2022-03-06 23:24:39 --> Severity: Warning --> Trying to access array offset on value of type null C:\xampp\htdocs\pro-salud.github.io\easyappointments\application\controllers\Backend_api.php 164
+ERROR - 2022-03-06 23:24:39 --> Severity: Warning --> Trying to access array offset on value of type null C:\xampp\htdocs\pro-salud.github.io\easyappointments\application\controllers\Backend_api.php 164
+ERROR - 2022-03-06 23:25:39 --> Severity: Warning --> Trying to access array offset on value of type null C:\xampp\htdocs\pro-salud.github.io\easyappointments\application\controllers\Backend_api.php 164
+ERROR - 2022-03-06 23:25:39 --> Severity: Warning --> Trying to access array offset on value of type null C:\xampp\htdocs\pro-salud.github.io\easyappointments\application\controllers\Backend_api.php 164
+ERROR - 2022-03-06 23:26:39 --> Severity: Warning --> Trying to access array offset on value of type null C:\xampp\htdocs\pro-salud.github.io\easyappointments\application\controllers\Backend_api.php 164
+ERROR - 2022-03-06 23:26:39 --> Severity: Warning --> Trying to access array offset on value of type null C:\xampp\htdocs\pro-salud.github.io\easyappointments\application\controllers\Backend_api.php 164
+ERROR - 2022-03-06 23:27:39 --> Severity: Warning --> Trying to access array offset on value of type null C:\xampp\htdocs\pro-salud.github.io\easyappointments\application\controllers\Backend_api.php 164
+ERROR - 2022-03-06 23:27:39 --> Severity: Warning --> Trying to access array offset on value of type null C:\xampp\htdocs\pro-salud.github.io\easyappointments\application\controllers\Backend_api.php 164
+ERROR - 2022-03-06 23:28:39 --> Severity: Warning --> Trying to access array offset on value of type null C:\xampp\htdocs\pro-salud.github.io\easyappointments\application\controllers\Backend_api.php 164
+ERROR - 2022-03-06 23:28:39 --> Severity: Warning --> Trying to access array offset on value of type null C:\xampp\htdocs\pro-salud.github.io\easyappointments\application\controllers\Backend_api.php 164
+ERROR - 2022-03-06 23:29:39 --> Severity: Warning --> Trying to access array offset on value of type null C:\xampp\htdocs\pro-salud.github.io\easyappointments\application\controllers\Backend_api.php 164
+ERROR - 2022-03-06 23:29:39 --> Severity: Warning --> Trying to access array offset on value of type null C:\xampp\htdocs\pro-salud.github.io\easyappointments\application\controllers\Backend_api.php 164
+ERROR - 2022-03-06 23:30:19 --> Email could not been sent. Mailer Error (Line 166): Could not instantiate mail function.
+ERROR - 2022-03-06 23:30:19 --> #0 C:\xampp\htdocs\pro-salud.github.io\easyappointments\application\libraries\Notifications.php(94): EA\Engine\Notifications\Email->send_appointment_details(Array, Array, Array, Array, Array, Object(EA\Engine\Types\Text), Object(EA\Engine\Types\Text), Object(EA\Engine\Types\Url), Object(EA\Engine\Types\Email), Object(EA\Engine\Types\Text), 'America/Mexico_...')
+#1 C:\xampp\htdocs\pro-salud.github.io\easyappointments\application\controllers\Appointments.php(490): Notifications->notify_appointment_saved(Array, Array, Array, Array, Array, false)
+#2 C:\xampp\htdocs\pro-salud.github.io\easyappointments\vendor\codeigniter\framework\system\core\CodeIgniter.php(481): Appointments->ajax_register_appointment()
+#3 C:\xampp\htdocs\pro-salud.github.io\easyappointments\index.php(341): require_once('C:\\xampp\\htdocs...')
+#4 {main}
+ERROR - 2022-03-06 23:30:36 --> Severity: Warning --> Trying to access array offset on value of type null C:\xampp\htdocs\pro-salud.github.io\easyappointments\application\controllers\Backend_api.php 164
+ERROR - 2022-03-06 23:30:36 --> Severity: Warning --> Trying to access array offset on value of type null C:\xampp\htdocs\pro-salud.github.io\easyappointments\application\controllers\Backend_api.php 164
