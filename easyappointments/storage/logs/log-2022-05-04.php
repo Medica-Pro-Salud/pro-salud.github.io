56bcf02d738121604a17df0fc46852bd411261c6
Mike&rade <hoodloom07@outlook.es>
2022-05-03 18:22:19 -0500

    Configuración de colores back

---
+++ easyappointments/storage/logs/log-2022-05-04.php
@@ -0,0 +1,4 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2022-05-04 01:09:01 --> Severity: Warning --> mysqli::real_connect(): (HY000/2002): No se puede establecer una conexión ya que el equipo de destino denegó expresamente dicha conexión C:\xampp\htdocs\pro-salud.github.io\easyappointments\vendor\codeigniter\framework\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2022-05-04 01:09:01 --> Severity: error --> Exception: Unable to connect to the database. C:\xampp\htdocs\pro-salud.github.io\easyappointments\vendor\codeigniter\framework\system\database\DB_driver.php 433
